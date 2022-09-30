28a5e459e2871168dc5efc1c96950dcabb8e03ff
Sergio Viera <sergioviera@gmail.com>
2022-09-29 21:55:57 -0300

    primer commit

---
+++ instituto_educativo.sql
@@ -0,0 +1,379 @@
+-- MariaDB dump 10.19  Distrib 10.4.24-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: instituto_educativo
+-- ------------------------------------------------------
+-- Server version	10.4.24-MariaDB
+CREATE DATABASE `instituto_educativo`;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `alumno_curso_personal`
+--
+
+DROP TABLE IF EXISTS `alumno_curso_personal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `alumno_curso_personal` (
+  `id_alumno` int(11) NOT NULL,
+  `id_curso_personal` int(11) NOT NULL,
+  PRIMARY KEY (`id_alumno`,`id_curso_personal`),
+  KEY `id_curso_personal` (`id_curso_personal`),
+  CONSTRAINT `alumno_curso_personal_ibfk_1` FOREIGN KEY (`id_alumno`) REFERENCES `alumnos` (`id`),
+  CONSTRAINT `alumno_curso_personal_ibfk_2` FOREIGN KEY (`id_curso_personal`) REFERENCES `cursos_personales` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alumno_curso_personal`
+--
+
+LOCK TABLES `alumno_curso_personal` WRITE;
+/*!40000 ALTER TABLE `alumno_curso_personal` DISABLE KEYS */;
+INSERT INTO `alumno_curso_personal` VALUES (1,1),(2,1),(3,1),(4,1);
+/*!40000 ALTER TABLE `alumno_curso_personal` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `alumnos`
+--
+
+DROP TABLE IF EXISTS `alumnos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `alumnos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alumnos`
+--
+
+LOCK TABLES `alumnos` WRITE;
+/*!40000 ALTER TABLE `alumnos` DISABLE KEYS */;
+INSERT INTO `alumnos` VALUES (1,'Mario Dominguez'),(2,'Morena Diaz'),(3,'Belén Medina'),(4,'Jorge Moreira'),(5,'Martín Ríos'),(6,'Susana Gómez'),(7,'Carlos Smith'),(8,'Alejandro Perez');
+/*!40000 ALTER TABLE `alumnos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `aulas`
+--
+
+DROP TABLE IF EXISTS `aulas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `aulas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `codigo` varchar(255) NOT NULL,
+  `capacidad` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `aulas`
+--
+
+LOCK TABLES `aulas` WRITE;
+/*!40000 ALTER TABLE `aulas` DISABLE KEYS */;
+INSERT INTO `aulas` VALUES (1,'A1',50),(2,'A2',15),(3,'A3',30),(4,'A4',30);
+/*!40000 ALTER TABLE `aulas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categorias`
+--
+
+DROP TABLE IF EXISTS `categorias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categorias` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  `importe_por_hora` float NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categorias`
+--
+
+LOCK TABLES `categorias` WRITE;
+/*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
+INSERT INTO `categorias` VALUES (1,'Profesor Asociado',6175.5),(2,'Profesor Ordinario',4865.75);
+/*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cursos`
+--
+
+DROP TABLE IF EXISTS `cursos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cursos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_tema` int(11) NOT NULL,
+  `fecha_inicio` date NOT NULL,
+  `id_docente` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_docente` (`id_docente`),
+  KEY `id_tema` (`id_tema`),
+  CONSTRAINT `cursos_ibfk_1` FOREIGN KEY (`id_docente`) REFERENCES `docentes` (`id`),
+  CONSTRAINT `cursos_ibfk_2` FOREIGN KEY (`id_tema`) REFERENCES `temas` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cursos`
+--
+
+LOCK TABLES `cursos` WRITE;
+/*!40000 ALTER TABLE `cursos` DISABLE KEYS */;
+INSERT INTO `cursos` VALUES (1,2,'2022-09-01',1),(2,1,'2022-09-29',2);
+/*!40000 ALTER TABLE `cursos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cursos_empresariales`
+--
+
+DROP TABLE IF EXISTS `cursos_empresariales`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cursos_empresariales` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `precio_total` float NOT NULL,
+  `id_empresa` int(11) NOT NULL,
+  `cantidad_alumnos` int(11) NOT NULL,
+  `id_curso` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `cursos_empresariales` (`id_curso`),
+  KEY `id_empresa` (`id_empresa`),
+  CONSTRAINT `cursos_empresariales` FOREIGN KEY (`id_curso`) REFERENCES `cursos` (`id`),
+  CONSTRAINT `cursos_empresariales_ibfk_1` FOREIGN KEY (`id_empresa`) REFERENCES `empresas` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cursos_empresariales`
+--
+
+LOCK TABLES `cursos_empresariales` WRITE;
+/*!40000 ALTER TABLE `cursos_empresariales` DISABLE KEYS */;
+INSERT INTO `cursos_empresariales` VALUES (1,20500,1,20,1);
+/*!40000 ALTER TABLE `cursos_empresariales` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cursos_personales`
+--
+
+DROP TABLE IF EXISTS `cursos_personales`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cursos_personales` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `precio_por_alumno` float NOT NULL,
+  `id_aula` int(11) NOT NULL,
+  `id_curso` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `curso_personal` (`id_curso`),
+  KEY `id_aula` (`id_aula`),
+  CONSTRAINT `curso_personal` FOREIGN KEY (`id_curso`) REFERENCES `cursos` (`id`),
+  CONSTRAINT `cursos_personales_ibfk_1` FOREIGN KEY (`id_aula`) REFERENCES `aulas` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cursos_personales`
+--
+
+LOCK TABLES `cursos_personales` WRITE;
+/*!40000 ALTER TABLE `cursos_personales` DISABLE KEYS */;
+INSERT INTO `cursos_personales` VALUES (1,1500,1,2);
+/*!40000 ALTER TABLE `cursos_personales` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `docentes`
+--
+
+DROP TABLE IF EXISTS `docentes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `docentes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `docentes`
+--
+
+LOCK TABLES `docentes` WRITE;
+/*!40000 ALTER TABLE `docentes` DISABLE KEYS */;
+INSERT INTO `docentes` VALUES (1,'Sergio Viera'),(2,'Santiago Raggazzo'),(3,'Marcelo Badino');
+/*!40000 ALTER TABLE `docentes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `empresas`
+--
+
+DROP TABLE IF EXISTS `empresas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `empresas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  `direccion` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `empresas`
+--
+
+LOCK TABLES `empresas` WRITE;
+/*!40000 ALTER TABLE `empresas` DISABLE KEYS */;
+INSERT INTO `empresas` VALUES (1,'Next DDS','Mitre 2'),(2,'Mundo UML','San Martín 203');
+/*!40000 ALTER TABLE `empresas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `material`
+--
+
+DROP TABLE IF EXISTS `material`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `material` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(255) NOT NULL,
+  `costo` float NOT NULL,
+  `id_tema` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_tema` (`id_tema`),
+  CONSTRAINT `material_ibfk_1` FOREIGN KEY (`id_tema`) REFERENCES `temas` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `material`
+--
+
+LOCK TABLES `material` WRITE;
+/*!40000 ALTER TABLE `material` DISABLE KEYS */;
+INSERT INTO `material` VALUES (1,'Manual de Calidad',250,1),(2,'Manual de XP',250,2),(3,'Lapiz óptico',830,1),(4,'Diccionario',200,2),(5,'Manual de Calidad 2',250,1);
+/*!40000 ALTER TABLE `material` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `profesores`
+--
+
+DROP TABLE IF EXISTS `profesores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `profesores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `profesion` varchar(255) DEFAULT NULL,
+  `id_categoria` int(11) NOT NULL,
+  `id_docente` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `docente_profesor` (`id_docente`),
+  KEY `id_categoria` (`id_categoria`),
+  CONSTRAINT `docente_profesor` FOREIGN KEY (`id_docente`) REFERENCES `docentes` (`id`),
+  CONSTRAINT `profesores_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `profesores`
+--
+
+LOCK TABLES `profesores` WRITE;
+/*!40000 ALTER TABLE `profesores` DISABLE KEYS */;
+INSERT INTO `profesores` VALUES (1,'Ingeniero',1,1),(2,'Licenciado',2,2);
+/*!40000 ALTER TABLE `profesores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `socios`
+--
+
+DROP TABLE IF EXISTS `socios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `socios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `antiguedad` int(11) NOT NULL,
+  `valor_acordado` float NOT NULL,
+  `id_docente` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `socio_docente` (`id_docente`),
+  CONSTRAINT `socio_docente` FOREIGN KEY (`id_docente`) REFERENCES `docentes` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `socios`
+--
+
+LOCK TABLES `socios` WRITE;
+/*!40000 ALTER TABLE `socios` DISABLE KEYS */;
+INSERT INTO `socios` VALUES (1,5,1500,2);
+/*!40000 ALTER TABLE `socios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `temas`
+--
+
+DROP TABLE IF EXISTS `temas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `temas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  `duracion` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `temas`
+--
+
+LOCK TABLES `temas` WRITE;
+/*!40000 ALTER TABLE `temas` DISABLE KEYS */;
+INSERT INTO `temas` VALUES (1,'Programación en C++',12),(2,'Diseño de Sistemas',36);
+/*!40000 ALTER TABLE `temas` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-09-29 21:51:28
